f098bccd801d76aa893bda8ecd11747469df4d49
beverlyhills90 <sasadan800@gmail.com>
2026-07-01 13:00:02 +0200

    fix exeption raise

--- parser.py
+++ parser.py
@@ -36,23 +36,24 @@ class Hub(BaseModel):
 
 
 class MapData(BaseModel):
-    nb_drones: tuple[int, int]
+    nb_drones: int = Field(ge=1)
     start_hub_name: str = Field(min_length=1, max_length=100)
-    start_hub_cords: tuple = Field(ge=0)
+    start_hub_cords: tuple[int,int]
     start_hub_meta_data: dict
     end_hub: tuple[int, int]
-    end_hub_name: tuple[int, int]
+    end_hub_name: str
     end_hub_meta_data: dict
     hubs: list[Hub]
     connections: list[Connections]
 
-    @staticmethod
-    def parsing_from_file(file_path: str):
-        errors = []
+    @classmethod
+    def parsing_from_file(cls,file_path: str) -> "MapData":
         allowed_zones = ["normal", "blocked", "restricted", "priority"]
+
         main_data: dict[str, Any] = {}
         hubs = []
         connections = []
+        
         start_marker = False
         finish_marker = False
 
@@ -71,11 +72,12 @@ class MapData(BaseModel):
             elif hub_t == HUBS.END:
                 main_data["end_hub_name"] = hub_name
                 main_data["end_hub"] = hub_cord
-                main_data["end_hub"] = vars(Hub_MetaData(zone=zone, max_drones=max_drones, color=color))
+                main_data["end_hub_meta_data"] = vars(Hub_MetaData(zone=zone, max_drones=max_drones, color=color))
             else:
                 metadata = vars(Hub_MetaData(zone=zone, max_drones=max_drones, color=color))
                 tmp = Hub(hub_name=hub_name,hub_cords=hub_cord,hub_meta_data=metadata)
                 hubs.append(tmp)
+        
         def load_connections_to_list(connections_lst:list[str],max_link_capacity:int | None) -> None:
             conn = Connections(connection_from=connections_lst[0],connection_to=connections_lst[1],max_link_capacity=max_link_capacity)
             connections.append(conn)
@@ -95,42 +97,25 @@ class MapData(BaseModel):
                                 hub_t = HUBS.START
                                 start_marker = True
                             else:
-                                errors.append(
-                                    ParsingError("We need only one start hub")
-                                )
-                                continue
+                                raise ParsingError("We need only one start hub")
                         elif "end_hub" in line:
                             if not finish_marker:
                                 hub_t = HUBS.END
                                 finish_marker = True
                             else:
-                                errors.append(
-                                    ParsingError("We need only one finish hub")
-                                )
-                                continue
+                                raise ParsingError("We need only one finish hub")
                         tmp = line.split(":")[1].split()
                         hub_name = tmp[0]
                         try:
-                            if int(tmp[1]) < 0 or int(tmp[2]) < 0:
-                                errors.append(
-                                    ParsingError(f"Cords in {tmp[0]} is negative")
-                                )
-                                continue
-                            else:
-                                hub_cords = (int(tmp[1]), int(tmp[2]))
+                            hub_cords = (int(tmp[1]), int(tmp[2]))
                         except ValueError:
-                            errors.append(
-                                ParsingError(f"Invalid cords format in {hub_name}")
-                            )
+                            raise ParsingError(f"Invalid cords format in {hub_name}")
                         color = None
                         max_drones = None
                         zone = None
                         if len(tmp) > 3:
                             if len(tmp) > 6:
-                                errors.append(
-                                    ParsingError(f"Too much metadata in {hub_name}")
-                                )
-                                continue
+                                raise ParsingError(f"Too much metadata in {hub_name}")
                             color_str = next(
                                 (item for item in tmp[1:] if "color" in item), None
                             )
@@ -150,12 +135,7 @@ class MapData(BaseModel):
                                 ]
                             )
                             if successfully_parsed != total_incoming_params:
-                                errors.append(
-                                    ParsingError(
-                                        f"Invalid or unknown metadata in {hub_name}"
-                                    )
-                                )
-                                continue
+                                raise ParsingError(f"Invalid or unknown metadata in {hub_name}")
                             if color_str is not None:
                                 color = color_str.split("=")[1].replace("]", "")
                             if max_drones_str is not None:
@@ -164,25 +144,14 @@ class MapData(BaseModel):
                                         max_drones_str.split("=")[1].replace("]", "")
                                     )
                                     if max_drones < 0:
-                                        errors.append(
-                                            f"Invalid metadata in {tmp[0]}: max_drones is negative"
-                                        )
+                                        raise ParsingError(f"Invalid metadata in {tmp[0]}: max_drones is negative"
+)
                                 except (IndexError, ValueError) as e:
-                                    errors.append(
-                                        ParsingError(
-                                            f"Invalid metadata in {tmp[0]}: {e}"
-                                        )
-                                    )
-                                    continue
+                                        raise ParsingError(f"Invalid metadata in {tmp[0]}: {e}")
                             if zone_str is not None:
                                 zone = zone_str.split("=")[1]
                                 if zone not in allowed_zones:
-                                    errors.append(
-                                        ParsingError(
-                                            f"Invalid metadata in {tmp[0]}: zone type is invalid"
-                                        )
-                                    )
-                                    continue
+                                    raise ParsingError(f"Invalid metadata in {tmp[0]}: zone type is invalid")
                             #print(hub_name, hub_cords, color, max_drones, zone)
                             load_hubs_to_dict(
                                 hub_name, hub_cords, color, max_drones, zone, hub_t
@@ -199,16 +168,21 @@ class MapData(BaseModel):
                             try:
                                 max_link_capacity = int(max_link_capacity_str.split("=")[1].replace("]",""))
                             except ValueError:
-                                errors.append(ParsingError(f"max_link_capacity in {tmp[0]} is invalid"))
+                                raise ParsingError(f"max_link_capacity in {tmp[0]} is invalid")
                         #print(connections_lst,max_link_capacity)
                         load_connections_to_list(connections_lst,max_link_capacity)
             main_data["hubs"] = hubs
             main_data["connections"] = connections
-            # clean_data = to_jsonable_python(main_data)
-            # print(json.dumps(clean_data, indent=2))
-        except (FileNotFoundError,PermissionError,IsADirectoryError,FileExistsError,UnicodeDecodeError) as e:
+
+            return cls.model_validate(main_data)
+
+
+        except (OSError) as e:
             print(f"File Erorr: {e}")
 
 
 if __name__ == "__main__":
-    MapData.parsing_from_file("maps/challenger/01_the_impossible_dream.txt")
+    map_data = MapData.parsing_from_file("maps/challenger/01_the_impossible_dream.txt")
+    clean_data = to_jsonable_python(map_data)
+    print(json.dumps(clean_data, indent=2))
+            
